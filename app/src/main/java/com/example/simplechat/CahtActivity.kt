55a6fb5801ec257616be0fc4c4bc6d5e316212cf
pnietopaez <pablonp67@gmail.com>
2023-04-21 10:41:00 +0200

    Hay

--- app/src/main/java/com/example/simplechat/CahtActivity.kt
+++ app/src/main/java/com/example/simplechat/CahtActivity.kt
@@ -2,14 +2,13 @@ package com.example.simplechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.simplechat.databinding.ActivityMainBinding
+import com.example.simplechat.databinding.ActivityCahtBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-
+class CahtActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCahtBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityCahtBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
